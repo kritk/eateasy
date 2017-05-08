97952b39467986d6769dda52d8977c54a621ed2c
kritk <kritmairuu@gmail.com>
2017-05-08 21:59:36 +0700

    create connection to mongodb

--- indexeateasy.php
+++ indexeateasy.php
@@ -1,2 +1,12 @@
 <?php
   echo 'Hello eateasy!';
+
+  // connect to mongodb
+   $m = new MongoClient("mongodb://krit:<PASSWORD>@cluster0-shard-00-00-ebb3m.mongodb.net:27017,cluster0-shard-00-01-ebb3m.mongodb.net:27017,cluster0-shard-00-02-ebb3m.mongodb.net:27017/<DATABASE>?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin");
+	
+   echo "Connection to database successfully";
+   // select a database
+   $db = $m->mydb;
+   $collection = $db->createCollection("eateasy");
+	
+   echo "Database mydb selected";
